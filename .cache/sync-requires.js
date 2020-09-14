890360a61ceb3ada0068f81d86c6564647156885
Adriana Lima <adrianalimafm@gmail.com>
2020-09-14 10:04:57 -0300

    Fix react icons library

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,12 +5,12 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\Documents\\Dev\\Div House Blog\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("D:\\Documents\\Dev\\Div House Blog\\src\\pages\\404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("D:\\Documents\\Dev\\Div House Blog\\src\\pages\\about.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("D:\\Documents\\Dev\\Div House Blog\\src\\pages\\contact.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("D:\\Documents\\Dev\\Div House Blog\\src\\pages\\index.js"))),
-  "component---src-templates-post-post-jsx": hot(preferDefault(require("D:\\Documents\\Dev\\Div House Blog\\src\\templates\\Post\\post.jsx"))),
-  "component---src-templates-tag-tag-jsx": hot(preferDefault(require("D:\\Documents\\Dev\\Div House Blog\\src\\templates\\Tag\\tag.jsx")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\adriana_lima\\Downloads\\Gatsby-Blog\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\adriana_lima\\Downloads\\Gatsby-Blog\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\adriana_lima\\Downloads\\Gatsby-Blog\\src\\pages\\about.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Users\\adriana_lima\\Downloads\\Gatsby-Blog\\src\\pages\\contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\adriana_lima\\Downloads\\Gatsby-Blog\\src\\pages\\index.js"))),
+  "component---src-templates-post-post-jsx": hot(preferDefault(require("C:\\Users\\adriana_lima\\Downloads\\Gatsby-Blog\\src\\templates\\Post\\post.jsx"))),
+  "component---src-templates-tag-tag-jsx": hot(preferDefault(require("C:\\Users\\adriana_lima\\Downloads\\Gatsby-Blog\\src\\templates\\Tag\\tag.jsx")))
 }
 
